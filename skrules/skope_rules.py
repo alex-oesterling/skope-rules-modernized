3691bd7b494f4902760185d8997bd07b43dd9670
Alex Oesterling <57299397+alex-oesterling@users.noreply.github.com>
2025-07-30 18:24:47 -0400

    Update skope_rules.py

--- skrules/skope_rules.py
+++ skrules/skope_rules.py
@@ -5,10 +5,10 @@ import pandas
 import numbers
 from warnings import warn
 
-from sklearn.base import Estimator
+from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
-from sklearn.utils import indices_to_mask
+from sklearn.utils._mask import indices_to_mask
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble import BaggingClassifier, BaggingRegressor
 from sklearn.tree import _tree
@@ -19,7 +19,7 @@ INTEGER_TYPES = (numbers.Integral, np.integer)
 BASE_FEATURE_NAME = "__C__"
 
 
-class SkopeRules(Estimator):
+class SkopeRules(BaseEstimator):
     """An easy-interpretable classifier optimizing simple logical rules.
 
     Parameters
